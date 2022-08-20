f6591a2a6e2dcc543b481324626a14a6b5aaa8c2
Victor Chiapaikeo <vchiapaikeo@gmail.com>
2022-08-20 09:48:21 -0400

    Seed

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM apache/airflow:2.3.3-python3.10
---
+++ dags/example.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+# from airflow.operators.docker_operator import DockerOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    "owner": "airflow",
+    "description": "Use of the DockerOperator",
+    "depend_on_past": False,
+    "start_date": datetime(2022, 8, 19),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "example",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    start_dag = DummyOperator(task_id="start_dag")
+
+    end_dag = DummyOperator(task_id="end_dag")
+
+    t1 = BashOperator(task_id="print_current_date", bash_command="date")
+
+    # t2 = DockerOperator(
+    #     task_id="docker_command_sleep",
+    #     image="docker_image_task",
+    #     container_name="task___command_sleep",
+    #     api_version="auto",
+    #     auto_remove=True,
+    #     command="echo hello",
+    #     docker_url="unix://var/run/docker.sock",
+    #     network_mode="bridge",
+    # )
+
+    # t3 = DockerOperator(
+    #     task_id="docker_command_hello",
+    #     image="docker_image_task",
+    #     container_name="task___command_hello",
+    #     api_version="auto",
+    #     auto_remove=True,
+    #     command="/bin/sleep 40",
+    #     docker_url="unix://var/run/docker.sock",
+    #     network_mode="bridge",
+    # )
+
+    t4 = BashOperator(task_id="print_hello", bash_command='echo "hello world"')
+
+    start_dag >> t1
+
+    t1 >> t4
+    # t1 >> t2 >> t4
+    # t1 >> t3 >> t4
+
+    t4 >> end_dag
